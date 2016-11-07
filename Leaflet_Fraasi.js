8c5f8e65e8d6a5c6a59c0d1ceaaa4d0db4861b10
Fraasi <Fraasi@users.noreply.github.com>
2016-11-07 21:17:32 +0200

    Add files via upload

--- Leaflet_Fraasi.js
+++ Leaflet_Fraasi.js
@@ -12,9 +12,13 @@
 	darkmap = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'});
 	
-var map = L.map('map', {fullscreenControl:{
-    pseudoFullscreen: true}, 
-	center: [61.50, 23.75],zoom: 11, layers: [OSM],  contextmenu: true, contextmenuWidth: 125, maxZoom: 19,
+var map = L.map('map', {
+	fullscreenControl : {pseudoFullscreen: true},
+	center: [61.50, 23.75],
+	zoom: 11,
+	layers: [OSM],
+	contextmenu: true,
+	contextmenuWidth: 125, maxZoom: 19,
     contextmenuItems: [{
         text: 'Show coordinates',
         callback: showCoordinates
@@ -41,7 +45,7 @@ var map = L.map('map', {fullscreenControl:{
 			function zoomIn (e) {
 				map.zoomIn(4);}
 			function zoomOut (e) {
-				map.zoomOut(6);}
+				map.zoomOut(5);}
 			function setView (e) {
 				map.setView([61.5, 23.75], 13);}
 
@@ -57,23 +61,43 @@ L.control.layers(baseMaps).addTo(map);
 //PLUGINS
 //Geocoder
 var osmGeocoder = new L.Control.OSMGeocoder({
-	collapsed: false,
-	position: 'bottomleft',
-	text: 'Haje!',
+		collapsed: false,
+		position: 'bottomleft',
+		text: 'Haje!',
 	});
 map.addControl(osmGeocoder);
 
 //MeasureDistance
-L.control.measure({primaryLengthUnit: 'meters', secondaryLengthUnit: 'kilometers', primaryAreaUnit: 'sqmeters', secondaryAreaUnit: 'hectares', activeColor: '#345CAE', completedColor: '#0526A3', position: 'bottomleft'}).addTo(map);
+L.control.measure({primaryLengthUnit: 'meters',
+	secondaryLengthUnit: 'kilometers',
+	primaryAreaUnit: 'sqmeters',
+	secondaryAreaUnit: 'hectares',
+	activeColor: '#345CAE',
+	completedColor: '#0526A3',
+	position: 'bottomleft'
+}).addTo(map);
 
 //Minimap		
 var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data &copy; OpenStreetMap contributors';
-var osm2 = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 13, attribution: osmAttrib });
-var miniMap = new L.Control.MiniMap(osm2, {minimized: true, toggleDisplay: true, position: 'bottomleft', zoomLevelOffset: -6 }).addTo(map);
+var osm2 = new L.TileLayer(osmUrl, {
+		minZoom: 0,
+		maxZoom: 13,
+		attribution: osmAttrib
+	});
+var miniMap = new L.Control.MiniMap(osm2, {
+		minimized: true,
+		toggleDisplay: true,
+		position: 'bottomleft',
+		zoomLevelOffset: -6
+	}).addTo(map);
 
 //Scale
-L.control.scale({position: 'bottomright', imperial: false, maxWidth: 150}).addTo(map);
+L.control.scale({
+	position: 'bottomright',
+	imperial: false,
+	maxWidth: 150
+}).addTo(map);
 
 //Elevation box
 var el = L.control.elevation({
@@ -121,9 +145,10 @@ fbutton.addEventListener('click', function(){
 		fbutton.innerHTML = '&darr;';
 		mapHeight.style.height = '420px';
 	}
-}); 
+});
 
 // Toggle showall button
+var tracks = {};
 var showAll = document.getElementById("all");
 showAll.onclick = function () {
 	for (var reittiId in tracks) {
@@ -132,50 +157,54 @@ showAll.onclick = function () {
 			map.addLayer(gpx);
 		} else {
 			map.removeLayer(gpx);
-		} 
+		}
 	}	
 	if (showAll.innerHTML === 'Show all'){
 		showAll.innerHTML = 'Hide all';
 	} else {
 		showAll.innerHTML = 'Show all';
-	}	
+	}
 };
 
-var tampere = L.marker([61.4985, 23.764]).bindPopup('This is Tampere.<br>Choose routes in layercontrol on the right. <br>Click route on the map to get info into the hidden footer.<br>Use all the plugins.<br>Also right click for some extra options.<br>Profit...').addTo(map);
-var  layerControl = new L.control.layers(null, null, {collapsed: true}).addTo(map);
+var tampere = L.marker([61.4985, 23.764]).bindPopup('This is Tampere.<br>Choose routes in layercontrol on the right. <br>Click route on the map to get info into the hidden footer.<br>Use all the plugins.<br>Also right click for some extra options.<br>Profit!').addTo(map);
+
+var layerControl = new L.control.layers(null, null, {collapsed: true}).addTo(map);
 layerControl.addOverlay(tampere, "<b>Tampere</b><hr>");
 
 var icons =  {
     startIconUrl: 'images/pin-icon-start.png',
     endIconUrl: 'images/pin-icon-end.png',
-    shadowUrl: 'images/pin-shadow.png'};
-
-var tracks = {};
-var gpxdir = "gpx/"; 
+    shadowUrl: 'images/pin-shadow.png'
+	};
 
 // Few example GPX files, I have about 30 tracks here at the moment...	
+var gpxdir = "gpx/";
 var gpxfiles =  [
-'2015-08-25T14-25-12_Reuharinniemi.gpx',
-'2015-08-03T12-07-29_Pyhajarvikierros.gpx',
-'2016-07-30T15-18-58_Suolijärvi.gpx'
-];
+	'2015-08-25T14-25-12_Reuharinniemi.gpx',
+	'2015-08-03T12-07-29_Pyhajarvikierros.gpx',
+	'2016-07-30T15-18-58_Suolijärvi.gpx'
+	];
 
-//The great loopdidoo starts
-for (var l = 0; l < gpxfiles.length; l++){ 
-	var urli = gpxdir + gpxfiles[l];
-	var gpx = new L.GPX(urli, {async: true, marker_options: icons});
-	var n = l + 1;	
+//The great loopdidoo had to be put into function to get gpxOnClick working for chosen file
+function loopdidoo() {
+	for (var l = 0; l < gpxfiles.length; l++){
+		var urli = gpxdir + gpxfiles[l];
+		var gpx = new L.GPX(urli, {
+				async: true,
+				marker_options: icons
+			});
+		var n = l + 1;	
 
-    layerControl.addOverlay(gpx, "" + n++ + ". " + urli.slice(24, -4));
-	gpxOnClick();
-	tracks[L.stamp(gpx)] = gpx;
+		layerControl.addOverlay(gpx, "" + n++ + ". " + urli.slice(24, -4));
+		tracks[L.stamp(gpx)] = gpx;
+		gpxOnClick(gpx);
+	}
 }
 
-function gpxOnClick(g) {	
+function gpxOnClick(gpx) {	
 	gpx.addEventListener("click", function(event){	
 		var gpxClicked = event.target;
-
-	    map.fitBounds(gpxClicked.getBounds());
+		map.fitBounds(gpxClicked.getBounds());
 		setElevationData(gpxClicked);
 
 		function _id(id) {return document.getElementById(id);}
@@ -195,7 +224,7 @@ function gpxOnClick(g) {
 	});	 
 };
 
-function setElevationData(g){
+function setElevationData(gpx){
 	el.clear();
 	for (var gpxLayer in gpx._layers){
 		for (var trackLayer in gpx._layers[gpxLayer]._layers){
@@ -208,40 +237,36 @@ function setElevationData(g){
 var chosengpx = document.getElementById("choosegpx");
  	      
 function chosenFile(){
-	var track = false;
+	var gpx = false;
 	var txt = "";
 
     if ('files' in chosengpx) {
-            for (var i = 0; i < chosengpx.files.length; i++) {
-                txt += "<br><strong>" + (i+1) + ". file</strong><br>";
-                var file = chosengpx.files[i];
-					if ('name' in file) {
-						txt += "<strong>name: </strong>" + file.name + "<br>";
-					}
-					if ('size' in file) {
-						txt += "<b>  size: </b>" + Math.round((file.size / 1024)) + " kb <br>";
-					}
+		for (var i = 0; i < chosengpx.files.length; i++) {
+			txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+			var file = chosengpx.files[i];
+			
+			if ('name' in file) {
+				txt += "<strong>name: </strong>" + file.name + "<br>";
+			}
+			if ('size' in file) {
+				txt += "<b>  size: </b>" + Math.round((file.size / 1024)) + " kb <br>";
+			}
 
 			var objectURL = window.URL.createObjectURL(file);
-			
-console.log('objectURL: ' + objectURL, file);
-
-var track = new L.GPX(objectURL, {async: true, marker_options: icons}).on('loaded', function(e) {
-
-console.log('this: ' + this,  e.target);
-        var gpx = e.target;
-		map.fitBounds(gpx.getBounds());
-		gpxOnClick(e);
-		layerControl.addOverlay(track, "" + file.name);
-   tracks[L.stamp(gpx)] = gpx;
-   window.URL.revokeObjectURL(file);
-}).addTo(map);
-            }
-        }
-    	else {
-            txt += "The files property is not supported by your browser!";
-            txt  += "<br>The path of the selected file: " + file.value; 
-        }
-    document.getElementById("nimi").innerHTML = txt;
+			var gpx = new L.GPX(objectURL, {
+					async: true,
+					marker_options: icons
+				}).on('loaded', function(e) {
+					var gpx = e.target;
+					layerControl.addOverlay(gpx, "" + file.name);
+					tracks[L.stamp(gpx)] = gpx;
+					gpxOnClick(gpx);
+					map.fitBounds(gpx.getBounds());
+					window.URL.revokeObjectURL(file);
+				}).addTo(map);
+		}
+	}
+	document.getElementById("nimi").innerHTML = txt;
 	document.getElementById("title").innerHTML = "<b>" + file.name +"</b>";	
 }
+loopdidoo();
