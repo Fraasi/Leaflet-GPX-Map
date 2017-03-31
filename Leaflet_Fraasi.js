cda25e57a7df470fc57d02a1b74a1a77c02ffdc7
Fraasi <doofassi@gmail.com>
2017-03-31 17:01:40 +0300

    Upload to rightclick & many other code improvements

--- Leaflet_Fraasi.js
+++ Leaflet_Fraasi.js
@@ -1,7 +1,11 @@
 //Le Map
-	var OSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var OSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'}),
+	
+	OSM_DE = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright/de">OpenStreetMap</a>'}),
 	
 	landMap = L.tileLayer('https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://thunderforest.com/">Thunderforest</a>'}),
@@ -22,49 +26,56 @@ var map = L.map('map', {
     contextmenuItems: [{
         text: 'Show coordinates',
         callback: showCoordinates
-    }, {
+		}, {
         text: 'Center map here',
         callback: centerMap
-    }, '-', {
+		}, '-', {
         text: 'Zoom in',
         icon: 'images/zoom-in.png',
         callback: zoomIn
-    }, {
+		}, {
         text: 'Zoom out',
         icon: 'images/zoom-out.png',
         callback: zoomOut
-    }, '-', {
+		}, '-', {
+		text: 'Upload .GPX file',
+		callback: uploadGPX
+		}, {
 		text: '------Go Home------',
-		callback: setView
-	}
-	]});
-			function showCoordinates (e) {
-				prompt(e.latlng, e.latlng);}
-			function centerMap (e) {
-				map.panTo(e.latlng), 11;}
-			function zoomIn (e) {
-				map.zoomIn(4);}
-			function zoomOut (e) {
-				map.zoomOut(5);}
-			function setView (e) {
-				map.setView([61.5, 23.75], 13);}
+		callback: goHome
+		}
+	]
+});
 
+function showCoordinates(e) {
+	prompt(e.latlng, e.latlng);}
+function centerMap(e) {
+	map.panTo(e.latlng), 11;}
+function zoomIn() {
+	map.zoomIn(4);}
+function zoomOut() {
+	map.zoomOut(5);}
+function goHome() {
+	map.setView([61.5, 23.75], 13);}
+function uploadGPX() {
+	chosengpx.click();}
+				
 var baseMaps = {
 	"OSM": OSM,
+	"OSM_DE": OSM_DE,
 	"Landscape": landMap,
 	"Cyclemap": cyclemap,
 	"Darkmap": darkmap
 };
-	
 L.control.layers(baseMaps).addTo(map);
 
 //PLUGINS
 //Geocoder
 var osmGeocoder = new L.Control.OSMGeocoder({
-		collapsed: false,
-		position: 'bottomleft',
-		text: 'Haje!',
-	});
+	collapsed: false,
+	position: 'bottomleft',
+	text: 'Haje!',
+});
 map.addControl(osmGeocoder);
 
 //MeasureDistance
@@ -79,18 +90,18 @@ L.control.measure({primaryLengthUnit: 'meters',
 
 //Minimap		
 var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var osmAttrib='Map data &copy; OpenStreetMap contributors';
-var osm2 = new L.TileLayer(osmUrl, {
-		minZoom: 0,
-		maxZoom: 13,
-		attribution: osmAttrib
-	});
-var miniMap = new L.Control.MiniMap(osm2, {
-		minimized: true,
-		toggleDisplay: true,
-		position: 'bottomleft',
-		zoomLevelOffset: -6
-	}).addTo(map);
+var osmMini = new L.TileLayer(osmUrl, {
+	minZoom: 0,
+	maxZoom: 13,
+	attribution: 'Map data &copy; OpenStreetMap contributors'
+});
+
+var miniMap = new L.Control.MiniMap(osmMini, {
+	minimized: true,
+	toggleDisplay: true,
+	position: 'bottomleft',
+	zoomLevelOffset: -6
+}).addTo(map);
 
 //Scale
 L.control.scale({
@@ -100,6 +111,8 @@ L.control.scale({
 }).addTo(map);
 
 //Elevation box
+L.Browser.touch = L.Browser.mobile; // temp fix to get elevation hover working on chrome
+
 var el = L.control.elevation({
 	theme: "steelblue-theme",
 	width: 300,
@@ -109,40 +122,17 @@ var el = L.control.elevation({
 });
 el.addTo(map);	
 
-// Toggle Choose-box
-var cbutton = document.getElementById('showHide');
-
-cbutton.addEventListener('click', function(){
-	var cbox = document.getElementById('choosebox');
-	var mapWidth = document.getElementById('map');
-	var header = document.getElementById('hedder');
-	
-	if(cbox.style.display == 'block'){
-		cbox.style.display = 'none';
-		cbutton.innerHTML = '&larr;';
-		mapWidth.style.width = '98%';
-		header.style.width = '97.7%'; 
-	} else {
-		cbox.style.display = 'block';
-		cbutton.innerHTML = '&rarr;';
-		mapWidth.style.width = '75%';
-		header.style.width = '75%';
-	}
-});
-
 //Toggle Footer-box
-var fbutton = document.getElementById('showfooter');
-fbutton.addEventListener('click', function(){
+var footerButton = document.getElementById('showfooter');
+footerButton.addEventListener('click', function(){
 	var foot = document.getElementById('footsie');
 	var mapHeight = document.getElementById('map');
 
 	if(foot.style.display == 'block'){
 		foot.style.display = 'none';
-		fbutton.innerHTML = '&uarr;';
 		mapHeight.style.height = '500px';
 	} else {
 		foot.style.display = 'block';
-		fbutton.innerHTML = '&darr;';
 		mapHeight.style.height = '420px';
 	}
 });
@@ -151,22 +141,22 @@ fbutton.addEventListener('click', function(){
 var tracks = {};
 var showAll = document.getElementById("all");
 showAll.onclick = function () {
-	for (var reittiId in tracks) {
-		var gpx = tracks[reittiId];
+	for (var trackiId in tracks) {
+		var gpx = tracks[trackiId];
 		if(showAll.innerHTML === 'Show all') {
 			map.addLayer(gpx);
 		} else {
 			map.removeLayer(gpx);
 		}
-	}	
-	if (showAll.innerHTML === 'Show all'){
+	}
+	if (showAll.innerHTML === 'Show all') {
 		showAll.innerHTML = 'Hide all';
 	} else {
 		showAll.innerHTML = 'Show all';
 	}
 };
 
-var tampere = L.marker([61.4985, 23.764]).bindPopup('This is Tampere.<br>Choose routes in layercontrol on the right. <br>Click route on the map to get info into the hidden footer.<br>Use all the plugins.<br>Also right click for some extra options.<br>Profit!').addTo(map);
+var tampere = L.marker([61.4985, 23.764]).bindPopup('This is Tampere.<br>Choose routes in layercontrol on the right. <br>Click route on the map to get info into the hidden footer.<br>Right click to load your own GPX and for some other options.<br>Use all the plugins.').addTo(map);
 
 var layerControl = new L.control.layers(null, null, {collapsed: true}).addTo(map);
 layerControl.addOverlay(tampere, "<b>Tampere</b><hr>");
@@ -182,10 +172,10 @@ var gpxdir = "gpx/";
 var gpxfiles =  [
 	'2015-08-25T14-25-12_Reuharinniemi.gpx',
 	'2015-08-03T12-07-29_Pyhajarvikierros.gpx',
-	'2016-07-30T15-18-58_Suolijärvi.gpx'
+	'2016-07-30T15-18-58_Suolijarvi.gpx'
 	];
 
-//The great loopdidoo had to be put into function to get gpxOnClick working for chosen file
+//The great loopdidoo had to be put into separate functions to get gpxOnClick working for chosen file
 function loopdidoo() {
 	for (var l = 0; l < gpxfiles.length; l++){
 		var urli = gpxdir + gpxfiles[l];
@@ -193,36 +183,40 @@ function loopdidoo() {
 				async: true,
 				marker_options: icons
 			});
-		var n = l + 1;	
-
-		layerControl.addOverlay(gpx, "" + n++ + ". " + urli.slice(24, -4));
+		// console.log(gpx.get_name(), gpx._info.name); // can't get name, oh why?
+		//try .on('loaded') next time around
+		layerControl.addOverlay(gpx, `${l + 1}. ${urli.slice(24, -4)}`);
 		tracks[L.stamp(gpx)] = gpx;
 		gpxOnClick(gpx);
 	}
 }
 
 function gpxOnClick(gpx) {	
-	gpx.addEventListener("click", function(event){	
-		var gpxClicked = event.target;
-		map.fitBounds(gpxClicked.getBounds());
-		setElevationData(gpxClicked);
-
-		function _id(id) {return document.getElementById(id);}
-	
-		// header info 
-		_id('title').textContent = gpxClicked.get_name() + " - " + gpxClicked.get_start_time().toDateString() + ', ' + gpxClicked.get_start_time().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})
-		
-		//footer info
-		_id('distance').textContent = gpxClicked.get_distance().toFixed(0) / 1000;
-		_id('duration').textContent = gpxClicked.get_duration_string(gpxClicked.get_moving_time().toFixed(4));
-		_id('avgspeed').textContent = gpxClicked.get_moving_speed().toFixed(0);
-	   	_id('elevation-gain').textContent = gpxClicked.get_elevation_gain().toFixed(0);
-		_id('elevation-loss').textContent = gpxClicked.get_elevation_loss().toFixed(0);
-		_id('elevation-net').textContent  = gpxClicked.get_elevation_gain().toFixed(0) - gpxClicked.get_elevation_loss().toFixed(0);	
-		var stoptimee = gpxClicked.get_total_time() - gpxClicked.get_moving_time();
-		_id('stoptime').textContent = (stoptimee * 1.66667e-5).toFixed();
-	});	 
+	gpx.addEventListener("click", setInfo);	 
 };
+
+function setInfo(click, upload){	
+	var gpxClicked = upload || click.target;
+	map.fitBounds(gpxClicked.getBounds());
+	setElevationData(gpxClicked);
+
+	function _id(id) {return document.getElementById(id);}
+
+	// header info 
+	_id('title').textContent = gpxClicked.get_name() + " - " + gpxClicked.get_start_time().toDateString() + ', ' + gpxClicked.get_start_time().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
+	
+	//footer info
+	_id('distance').textContent = gpxClicked.get_distance().toFixed(0) / 1000;
+	_id('duration').textContent = gpxClicked.get_duration_string(gpxClicked.get_moving_time().toFixed(4));
+	_id('avgspeed').textContent = gpxClicked.get_moving_speed().toFixed(0);
+	_id('elevation-gain').textContent = gpxClicked.get_elevation_gain().toFixed(0);
+	_id('elevation-loss').textContent = gpxClicked.get_elevation_loss().toFixed(0);
+	_id('elevation-net').textContent  = gpxClicked.get_elevation_gain().toFixed(0) - gpxClicked.get_elevation_loss().toFixed(0);	
+	var stoptimee = gpxClicked.get_total_time() - gpxClicked.get_moving_time();
+	// _id('stoptime').textContent = (stoptimee * 1.66667e-5).toFixed();
+	_id('stoptime').textContent = (stoptimee / 60000).toFixed();
+	_id('fileName').textContent = gpxClicked.fileName || gpxClicked._gpx.slice(4);
+}
 
 function setElevationData(gpx){
 	el.clear();
@@ -238,35 +232,25 @@ var chosengpx = document.getElementById("choosegpx");
  	      
 function chosenFile(){
 	var gpx = false;
-	var txt = "";
 
     if ('files' in chosengpx) {
-		for (var i = 0; i < chosengpx.files.length; i++) {
-			txt += "<br><strong>" + (i+1) + ". file</strong><br>";
-			var file = chosengpx.files[i];
-			
-			if ('name' in file) {
-				txt += "<strong>name: </strong>" + file.name + "<br>";
-			}
-			if ('size' in file) {
-				txt += "<b>  size: </b>" + Math.round((file.size / 1024)) + " kb <br>";
-			}
+		var file = chosengpx.files[0];
 
-			var objectURL = window.URL.createObjectURL(file);
-			var gpx = new L.GPX(objectURL, {
-					async: true,
-					marker_options: icons
-				}).on('loaded', function(e) {
-					var gpx = e.target;
-					layerControl.addOverlay(gpx, "" + file.name);
-					tracks[L.stamp(gpx)] = gpx;
-					gpxOnClick(gpx);
-					map.fitBounds(gpx.getBounds());
-					window.URL.revokeObjectURL(file);
-				}).addTo(map);
-		}
+		var objectURL = window.URL.createObjectURL(file);
+		var gpx = new L.GPX(objectURL, {
+				async: true,
+				marker_options: icons
+			})
+			.on('loaded', function(e) {
+				var gpx = e.target;
+				layerControl.addOverlay(gpx, `${Object.keys(tracks).length + 1}. ${gpx._info.name || file.name}`);
+				tracks[L.stamp(gpx)] = gpx;
+				tracks[L.stamp(gpx)].fileName = file.name;
+				gpxOnClick(gpx);
+				setInfo(null, gpx);
+				window.URL.revokeObjectURL(file);
+			}).addTo(map);
 	}
-	document.getElementById("nimi").innerHTML = txt;
-	document.getElementById("title").innerHTML = "<b>" + file.name +"</b>";	
 }
+
 loopdidoo();
